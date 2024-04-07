769052cd7ab01d7a10940031224ef9200100574b
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-04-07 14:44:39 -0400

    In Progress

--- DayType Project.cpp
+++ DayType Project.cpp
@@ -4,28 +4,15 @@
 
 using namespace std;
 
+int i;
+
 int main()
 {
-
-	for (int i = 0; i < 7; i++)
-		cout << DayType::DayofWeek[i] << " ";
-	cout << endl;
-
-	DayType::DayofWeek[0] = "Sun";
-	DayType::DayofWeek[1] = "Mon";
-	DayType::DayofWeek[2] = "Tue";
-	DayType::DayofWeek[3] = "Wed";
-	DayType::DayofWeek[4] = "Thu";
-	DayType::DayofWeek[5] = "Fri";
-	DayType::DayofWeek[6] = "Sat";
-
-	for (int i = 0; i < 7; i++)
-		cout << DayType::DayofWeek[i] << " ";
-	cout << endl;
-
-	cin >> DayofWeek[i];
-
-
+	
 	DayType Day;
-	Day.Print();
+	//Day.Print();
+
+	cout << "Please input the day of the week: ";
+	Day.NextDay();
+	//Day.PreviousDay();
 }
--- DayType.cpp
+++ DayType.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-	string DayType::DayofWeek[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+string DayType::DayofWeek[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-    void DayType::SetDay(string aDay)
-    {
-        int index = 0;
+void DayType::SetDay(string aDay)
+{
+    int index = 0;
         for (int i = 0; i < 7; i++)
         {
             if (DayofWeek[i] == aDay)
@@ -16,15 +16,115 @@ using namespace std;
                 break;
             }
         }
-        Day = DayofWeek[index];
+    Day = DayofWeek[index];
+}
+
+string DayType::NextDay()
+{
+    string CurrentDay;
+    cin >> CurrentDay;
+
+    if (CurrentDay == DayofWeek[0])
+    {
+        cout << "The next day of the week is " << DayofWeek[1] << endl;
+    }
+    
+    if (CurrentDay == DayofWeek[1])
+    {
+        cout << "The next day of the week is " << DayofWeek[2] << endl;
     }
 
-    void DayType::Print() const
+    if (CurrentDay == DayofWeek[2])
     {
-        cout << Day << endl;
+        cout << "The next day of the week is " << DayofWeek[3] << endl;
     }
 
-    DayType::DayType(string start)
+    if (CurrentDay == DayofWeek[3])
     {
-        SetDay(start);
+        cout << "The next day of the week is " << DayofWeek[4] << endl;
     }
+
+    if (CurrentDay == DayofWeek[4])
+    {
+        cout << "The next day of the week is " << DayofWeek[5] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[5])
+    {
+        cout << "The next day of the week is " << DayofWeek[6] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[6])
+    {
+        cout << "The next day of the week is " << DayofWeek[0] << endl;
+    }
+
+    else if(CurrentDay != DayofWeek[0, 1, 2, 3, 4, 5, 6])
+    {
+        cout << "Invalid entry.";
+    }
+
+    return Day;
+}
+
+string DayType::PreviousDay()
+{
+    string CurrentDay;
+
+    if (CurrentDay == DayofWeek[0])
+    {
+        cout << "The previous day of the week is " << DayofWeek[6] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[1])
+    {
+        cout << "The previous day of the week is " << DayofWeek[0] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[2])
+    {
+        cout << "The previous day of the week is " << DayofWeek[1] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[3])
+    {
+        cout << "The previous day of the week is " << DayofWeek[2] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[4])
+    {
+        cout << "The previous day of the week is " << DayofWeek[3] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[5])
+    {
+        cout << "The previous day of the week is " << DayofWeek[4] << endl;
+    }
+
+    if (CurrentDay == DayofWeek[6])
+    {
+        cout << "The previous day of the week is " << DayofWeek[5] << endl;
+    }
+
+    else if (CurrentDay != DayofWeek[0, 1, 2, 3, 4, 5, 6])
+    {
+        cout << "Invalid entry.";
+    }
+
+    return Day;
+}
+
+void DayType::AddDays(int NumDays)
+{
+
+}
+
+void DayType::Print() const
+{
+    cout << Day << endl;
+}
+
+DayType::DayType(string start)
+{
+    SetDay(start);
+}
